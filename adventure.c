b1c77f55aafbd99e2f763caa97eb0fd7c165b3c5
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-24 06:38:39 +0700

    📦 Added `SIGINT` signal handler

--- adventure.c
+++ adventure.c
@@ -1,7 +1,19 @@
+#include <stdlib.h>
+#include <signal.h>
+
+#include "./src/types.h"
 #include "./include/adventure_game.h"
 
-int main()
+void sigint_handler(i32 signal)
 {
+    clean_up_game();
+    exit(signal);
+}
+
+i32 main()
+{
+    signal(SIGINT, sigint_handler);
+
     init_game();
 
     game_loop();
