69d479408552705313933252e7625f4c004f5425
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-25 07:08:14 +0700

    📦 Added map and display to tui

--- src/adventure_game.c
+++ src/adventure_game.c
@@ -5,11 +5,19 @@
 #include "./types.h"
 
 #include "./config.h"
+#include "./adventure_game_intern.h"
 #include "./tui.h"
+
+GameGlobal *GAME_GLOBAL = nullptr;
+
+void game_init();
+void game_deinit();
 
 void init_game()
 {
     srand((u32)time(nullptr));
+
+    game_init();
 
     tui_init();
 }
@@ -20,10 +28,24 @@ void game_loop()
     {
         box(MAIN_WINDOW, 0, 0);
         tui_draw_title(MAIN_WINDOW, GAME_TITLE);
+        map_draw(MAIN_WINDOW, &(GAME_GLOBAL->m_map), 4, 4);
     }
 }
 
 void clean_up_game()
 {
+    game_deinit();
     tui_deinit();
+}
+
+void game_init()
+{
+    GAME_GLOBAL = calloc(0, sizeof(GameGlobal));
+    map_init(&(GAME_GLOBAL->m_map), DEFAULT_MAP_SIZE.x, DEFAULT_MAP_SIZE.y);
+}
+
+void game_deinit()
+{
+    map_deinit(&(GAME_GLOBAL->m_map));
+    free(GAME_GLOBAL);
 }
