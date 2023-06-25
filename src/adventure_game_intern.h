37e5c5597db77e96aa9a367d03a36b23985d1e4b
UnknownRori <68576836+UnknownRori@users.noreply.github.com>
2023-06-25 07:06:45 +0700

    📦 Added global game var

---
+++ src/adventure_game_intern.h
@@ -0,0 +1,26 @@
+/**
+ * @file adventure_game_intern.h
+ * @author UnknownRori (68576836+UnknownRori@users.noreply.github.com)
+ * @brief Internal header for adventure game
+ * @version 0.1
+ * @date 2023-06-24
+ *
+ * @copyright Copyright (c) 2023
+ *
+ */
+
+#pragma once
+
+#ifndef UNKNOWNRORI_ADVENTURE_GAME_INTERN_H
+#define UNKNOWNRORI_ADVENTURE_GAME_INTERN_H
+
+#include "./map.h"
+
+typedef struct game_global
+{
+    Map m_map;
+} GameGlobal;
+
+extern GameGlobal *GAME_GLOBAL;
+
+#endif // UNKNOWNRORI_ADVENTURE_GAME_INTERN_H
